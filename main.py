532a71dc3fea29a635e7cf0e94f68835daebc5b1
jxxxxharu <jxxxxharu@gmail.com>
2023-06-05 13:47:24 +0900

    blah

--- main.py
+++ main.py
@@ -207,29 +207,29 @@ def perform_keyword_extraction(data: TextData, top_n: int = 5):
     return {"keywords": keywords, "weights": weights}
 
 
-# @app.get("/similarity")
-# def perform_similarity(keyword1: str, keyword2: str):
-
-#     # 두 키워드 각각에 대해 BERT를 이용해 문장 임베딩 수행
-#     inputs1 = tokenizer2(keyword1, return_tensors='pt', padding=True, truncation=True)
-#     inputs2 = tokenizer2(keyword2, return_tensors='pt', padding=True, truncation=True)
-
-#     with torch.no_grad():
-#         outputs1 = model2(**inputs1)
-#         outputs2 = model2(**inputs2)
-
-#     # 문장 임베딩 결과의 평균을 취함
-#     embeddings1 = outputs1.last_hidden_state.mean(dim=1)
-#     embeddings2 = outputs2.last_hidden_state.mean(dim=1)
-
-#     # 두 임베딩 벡터 간의 코사인 유사도 계산
-#     similarity = cosine_similarity(embeddings1, embeddings2)
-
-#     return {'similarity': similarity.item()}
-
-@app.post("/similarity")
+@app.get("/similarity")
 def perform_similarity(keyword1: str, keyword2: str):
-    app_similarity(keyword1, keyword2)
+
+    # 두 키워드 각각에 대해 BERT를 이용해 문장 임베딩 수행
+    inputs1 = tokenizer2(keyword1, return_tensors='pt', padding=True, truncation=True)
+    inputs2 = tokenizer2(keyword2, return_tensors='pt', padding=True, truncation=True)
+
+    with torch.no_grad():
+        outputs1 = model2(**inputs1)
+        outputs2 = model2(**inputs2)
+
+    # 문장 임베딩 결과의 평균을 취함
+    embeddings1 = outputs1.last_hidden_state.mean(dim=1)
+    embeddings2 = outputs2.last_hidden_state.mean(dim=1)
+
+    # 두 임베딩 벡터 간의 코사인 유사도 계산
+    similarity = cosine_similarity(embeddings1, embeddings2)
+
+    return {'similarity': similarity.item()}
+
+# @app.post("/similarity")
+# def perform_similarity(keyword1: str, keyword2: str):
+#     app_similarity(keyword1, keyword2)
 
 
 
