0e22692d8bcc146b3f8ebd7ccc56860c8524eab2
jxxxxharu <jxxxxharu@gmail.com>
2023-06-10 15:11:53 +0900

    [Fix] ner (fine-tuning ver.)

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import torch
-from transformers import BertTokenizer, BertForTokenClassification, AutoTokenizer, AutoModel
+from transformers import BertTokenizer, BertForTokenClassification, AutoTokenizer, AutoModel, AutoModelForTokenClassification
 import numpy as np
 from keybert import KeyBERT
 from sentence_transformers import SentenceTransformer
 import uvicorn
 from utils.stopwords import stopwords
 from sklearn.metrics.pairwise import cosine_similarity
-# from similarity.app import app as app_similarity
 import re
 
 app = FastAPI()
@@ -22,253 +21,56 @@ class TextData(BaseModel):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertForTokenClassification.from_pretrained('bert-base-uncased')
-model.classifier = torch.nn.Linear(768, 15)
-
-model_dict = torch.load("./ner/models/pytorch_model.bin", map_location=device)
-model.load_state_dict(model_dict)
-model.to(device)
-model.eval()
 
 tokenizer2 = AutoTokenizer.from_pretrained('bert-base-uncased')
 model2 = AutoModel.from_pretrained('bert-base-uncased')
 
-entity_tags = ['FOOD', 'TECH', 'ENT', 'CHARITY', 'ART', 'OUT', 'MUSIC']
 
-# 각 tag를 고유 id(정수값)로 매핑
-unique_tags = set()
-for tag in entity_tags:
-    unique_tags.add('B-' + tag)
-    unique_tags.add('I-' + tag)
+class TextData2(BaseModel):
+    input: str
 
-unique_tags.add('O')
-
-tag2id = {tag: id for id, tag in enumerate(unique_tags)}
-id2tag = {id: tag for tag, id in tag2id.items()}
-
-# def encode_data(tokens_docs, tag_docs, max_seq_length):
-#     # Initialize the tokenizer
-#     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    
-#     encoded_inputs = []
-#     encoded_tags = []
-    
-#     for tokens in tokens_docs:
-#         # Convert tokens to input encoding
-#         encoded_input = tokenizer.encode_plus(tokens,
-#                                               truncation=True,
-#                                               padding='max_length',
-#                                               max_length=max_seq_length,
-#                                               return_attention_mask=True,
-#                                               return_tensors='pt')
-#         encoded_inputs.append(encoded_input)
-    
-#     for tags in tag_docs:
-#         # Convert tag labels to input encoding
-#         encoded_tag = [tag2id[tag] for tag in tags]
-        
-#         # Truncate or pad the tag sequence
-#         if len(encoded_tag) > max_seq_length:
-#             encoded_tag = encoded_tag[:max_seq_length]
-#         else:
-#             encoded_tag += [0] * (max_seq_length - len(encoded_tag))
-        
-#         encoded_tags.append(encoded_tag)
-    
-#     # Convert encoded inputs to tensors
-#     input_ids = torch.cat([encoded_input['input_ids'] for encoded_input in encoded_inputs], dim=0)
-#     attention_masks = torch.cat([encoded_input['attention_mask'] for encoded_input in encoded_inputs], dim=0)
-    
-#     # Convert encoded tags to tensors
-#     tags_ids = torch.tensor(encoded_tags)
-    
-#     return input_ids, attention_masks, tags_ids
-
-def encode_input_data(tokens, max_seq_length):
-    # Convert tokens to input encoding
-    encoded_input = tokenizer.encode_plus(tokens,
-                                        #   is_pretokenized=True, # Indicate that input is tokenized
-                                          add_special_tokens=True, # 특수 토큰([CLS], [SEP]) 추가 여부
-                                          max_length=max_seq_length, # 최대 길이 설정
-                                          padding='max_length', # 문장 길이가 짧을 때 패딩 적용
-                                          truncation=True, # 문장이 최대 길이보다 길 경우 자르기
-                                          return_attention_mask=True, # 어텐션 마스크 생성 여부
-                                          return_token_type_ids=True, # 세그먼트 인덱스 생성 여부
-                                          return_tensors='pt')
-    
-    input_ids = encoded_input['input_ids'].flatten().tolist()
-    attention_mask = encoded_input['attention_mask'].flatten().tolist()
-    token_type_ids = encoded_input['token_type_ids'].flatten().tolist()
-    
-    # # Convert BIO tags to label IDs
-    # label_ids = [tokenizer.encode(tag, max_length=max_seq_length, add_special_tokens=False)[0] for tag in tags]
-    
-    return {"input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "token_type_ids": token_type_ids,
-            # "label_ids": label_ids  # encoded BIO tag list
-            }
-
-def save_bio_keywords(tokens, tags) -> dict:
-    assert len(tokens) == len(tags), "Length of tokens and predicted tags should be the same."
-
-    keyword_dict = {}
-    current_entity_tokens = []
-    current_entity_label = None
-
-    for i, tag in enumerate(tags):
-        token = tokens[i]
-        if token.startswith('##'):
-            if current_entity_tokens:
-                token = token[2:]  # remove '##'
-                current_entity_tokens[-1] += token  # merge with the previous token
-            continue
-
-        if tag.startswith('B'):
-            # save previous entity
-            if current_entity_label is not None:
-                keyword_dict[current_entity_label] = keyword_dict.get(current_entity_label, []) + [' '.join(current_entity_tokens)]
-            
-            # start new entity
-            current_entity_label = tag.split('-')[1]
-            current_entity_tokens = [token]
-        elif tag.startswith('I') and current_entity_label == tag.split('-')[1]:
-            # continue the entity
-            if current_entity_label is not None:
-                current_entity_tokens.append(token)
-        else:
-            # save the previous entity if it exists
-            if current_entity_label is not None:
-                keyword_dict[current_entity_label] = keyword_dict.get(current_entity_label, []) + [' '.join(current_entity_tokens)]
-            
-            # reset the current entity
-            current_entity_tokens = []
-            current_entity_label = None
-
-    # add the last entity if it exists
-    if current_entity_label is not None:
-        keyword_dict[current_entity_label] = keyword_dict.get(current_entity_label, []) + [' '.join(current_entity_tokens)]
-
-    return keyword_dict
-
-def preprocess_text(raw_text) -> str:
-    text = raw_text.lower()  # 소문자로
-    text = text.replace('-', ' ')
-    text = text.replace('\t', ' ')
-    text = text.replace('\n', ' ')
-
-    clean_text = ""
-    for char in text:
-        if char in 'qwertyuiopasdfghjklzxcvbnm .?!':
-            clean_text += char
-        else:
-            clean_text += ' '
-
-    clean_text = re.sub(' +',' ', clean_text) # delete extra spaces
-    clean_text = clean_text.strip()
-
-    return clean_text
-
-
-def tokenize(doc_str) -> list:
-    tokens = tokenizer.tokenize(doc_str)
-    return tokens
+model = AutoModelForTokenClassification.from_pretrained('./ner/models/')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# tokenizer = AutoTokenizer.from_pretrained('tokenizer_path')
+id2label = model.config.id2label  # mapping from label IDs to label names
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 @app.post("/ner_inference")
-def perform_ner_inference(data: TextData): 
-    model.eval()
-
-    text = data.text
-    preprocessed_doc = preprocess_text(text)
-    tokens = tokenize(preprocessed_doc)
-
-    res = encode_input_data(tokens, len(tokens))
-    input_ids = torch.tensor(res['input_ids']).unsqueeze(0).to(device)
-    attention_mask = torch.tensor(res['attention_mask']).unsqueeze(0).to(device)
-    token_type_ids = torch.tensor(res['token_type_ids']).unsqueeze(0).to(device)
+def ner_inference(sen: TextData2):
+    sentence = sen.input
     
-    with torch.no_grad():
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        
-    logits = outputs.logits
-    logits = logits.detach().cpu().numpy()
-    label_ids = token_type_ids.cpu().numpy()
+    inputs = tokenizer(sentence, padding='max_length', truncation=True, max_length=512, return_tensors="pt")
 
-    predictions = [list(p) for p in np.argmax(logits, axis=2)]
-    true_labels = label_ids.tolist()
+    # move to gpu
+    ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    # forward pass
+    outputs = model(ids, mask)
+    logits = outputs[0]
 
-    pred_tags = [list(tag2id.keys())[p_i] for p in predictions for p_i in p]
- 
-    keyword_vectors = save_bio_keywords(tokens, pred_tags)
+    print('>>', logits.shape)  # >> torch.Size([1, 512, 768])
+    print('>>', model.config.num_labels)  # >> 17
+    active_logits = logits.view(-1, model.config.num_labels) # shape (batch_size * seq_len, num_labels)
+    flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
 
-    # print(text)
-    print(len(tokens), tokens)
-    print(len(pred_tags), pred_tags, end='\n\n')
+    tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
+    token_predictions = [id2label[i] for i in flattened_predictions.numpy()]
+    wp_preds = list(zip(tokens, token_predictions)) # list of tuples. Each tuple = (wordpiece, prediction)
 
-    return {'keyword_vectors': keyword_vectors, 
-            'tokens': tokens,
-            'pred_tags': pred_tags,
-            'text': text}
+    word_level_predictions = []
+    for pair in wp_preds:
+        if (pair[0].startswith(" ##")) or (pair[0] in ['[CLS]', '[SEP]', '[PAD]']):
+            # skip prediction
+            continue
+        else:
+            word_level_predictions.append(pair[1])
 
-# def perform_ner_inference(data: TextData):
+    # we join tokens, if they are not special ones
+    str_rep = " ".join([t[0] for t in wp_preds if t[0] not in ['[CLS]', '[SEP]', '[PAD]']]).replace(" ##", "")
 
-#     # Tokenize input text
-#     tokens = data.text.split()
-#     input_ids, attention_masks, tags_ids = encode_data([tokens], [[]], len(tokens))
-
-#     # Move tensors to the appropriate device
-#     input_ids = input_ids.to(device)
-#     attention_masks = attention_masks.to(device)
-#     tags_ids = tags_ids.to(device)
-
-#     with torch.no_grad():
-#         outputs = model(
-#             input_ids=input_ids,
-#             attention_mask=attention_masks,
-#             token_type_ids=tags_ids
-#         )
-
-#     logits = outputs['logits']
-#     logits = logits.detach().cpu().numpy()
-#     label_ids = tags_ids.cpu().numpy()
-
-#     predictions = [list(p) for p in np.argmax(logits, axis=2)]
-#     pred_tags = [id2tag[p_i] for p in predictions for p_i in p]
-
-#     # flag = False
-#     # start, end = 0, 0
-#     # text_vector = []
-#     # entity_tags = []
-
-#     # for token, tag in zip(tokens, pred_tags):
-#     #     if tag == 'O':
-#     #         if flag:
-#     #             end = len(entity_tags)
-#     #             entity = ' '.join(tokens[start:end])
-#     #             text_vector.append(entity)
-#     #             flag = False
-#     #         entity_tags.append(tag)
-#     #     else:
-#     #         if not flag:
-#     #             start = len(entity_tags)
-#     #             flag = True
-#     #         entity_tags.append(tag)
-
-#     # # Check if an entity is still open at the end
-#     # if flag:
-#     #     entity = ' '.join(tokens[start:])
-#     #     text_vector.append(entity)
-
-#     # return {"entities": text_vector, "pred_tags": pred_tags}
-#     entity_vectors = save_bio_keywords(tokens, pred_tags)
-
-#     # # print(text)
-#     # print(len(tokens), tokens)
-#     # print(len(pred_tags), pred_tags, end='\n\n')
-
-#     return entity_vectors, pred_tags
-
+    return {"str_rep": str_rep,
+            "word_level_predictions": word_level_predictions,}
 
 stop_words = stopwords
 # print(stop_words)
